80bc2a172c589b17964253a1c91337be48279316
huynguyend191 <huynguyend191@gmail.com>
2020-03-02 11:31:14 +0700

    add mysql config

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const https = require('https');
+// const https = require('https');
+const http = require('http');
 const app = require('./app');
 const dotenv =  require('dotenv');
-const certOptions = require('./utils/httpsCert');
+// const certOptions = require('./utils/httpsCert');
 
-const server = https.createServer(certOptions ,app);
+// const server = https.createServer(certOptions ,app);
+const server = http.createServer(app);
 dotenv.config();
 
 server.listen(process.env.PORT);
