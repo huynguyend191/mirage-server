14464aee4e1e6a1513b93514f91c745dd50d1903
huynguyend191 <huynguyend191@gmail.com>
2020-03-04 18:02:27 +0700

    add sql init sciprt

---
+++ resources/sql/initRootAdmin.sql
@@ -0,0 +1,10 @@
+INSERT INTO `mirage`.`accounts`
+(`id`,
+`username`,
+`password`,
+`role`, `createdAt`, `updatedAt`)
+VALUES
+('fc3dbe11-6f70-4050-a466-e9a4a7a39f6b',
+'admin',
+'$2b$10$lzlJeEC.MHu0LqxIlpdtq.WZff4xExxcC0jYmysDYkcVOrOTlJsYi',
+1, now(), now());
