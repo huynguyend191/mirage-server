d1dbb21fbc732bb25f240be7f03731d633018ed6
huynguyend191 <huynguyend191@gmail.com>
2020-05-05 10:23:11 +0700

    finish record call histories on server

--- routes/callHistories.js
+++ routes/callHistories.js
@@ -4,6 +4,4 @@ const callHistoriesController = require('../controllers/callHistories');
 const checkAuth = require('../middlewares/checkAuth');
 const roles = require('../lib/constants/account').ROLES;
 
-router.post('/', checkAuth(roles.STUDENT), callHistoriesController.createDirCallHistories, callHistoriesController.uploadCallVideos, callHistoriesController.createCallHistory);
-
 module.exports = router;
